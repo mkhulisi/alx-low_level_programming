25be1fd46f0a54f8f9497d10441fac2653b25581
mkhulisi <fakudzemkhulisi@gmail.com>
2023-02-16 01:01:34 -0800

    Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line, fix betty documatation style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
  * main - This is entry point for the program
- * prints a string using the printf() function
+ * the function prints a string using the printf() function
  * Return: 0 if successfull a none zero integer otherwise
  */
 int main(void)
