881fb17c8b7c52a88ac1aa60fe3fd55e88860f54
mkhulisi <fakudzemkhulisi@gmail.com>
2023-02-17 04:44:38 -0800

    get the last digit of a number and test if its zero, more than 5 or less than 6, fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 
 	if (ld == 0)
 	{
-		printf("Last digit of %d is %d and is zero\n", n, ld);
+		printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
 	else if (ld < 6 && ld != 0)
 	{
@@ -26,7 +26,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is grater than 5\n", n, ld);
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
 	return (0);
 }
