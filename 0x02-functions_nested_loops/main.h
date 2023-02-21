a6559fb0ea4a28dc7784e52ce53eace0a1985235
mkhulisi <fakudzemkhulisi@gmail.com>
2023-02-21 12:15:03 -0800

    Print the alphabets 10 times

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,3 @@
-/**
- * print_alphabet - function prototype
- * prints alphabets in lower case
- * Return: void
- */
 void print_alphabet(void);
 void _putchar(char);
+void print_alphabet_x10(void);
