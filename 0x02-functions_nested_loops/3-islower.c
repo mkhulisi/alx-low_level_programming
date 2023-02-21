bc8cd1d73f0ae784412f297ec4c6bd162eb7268d
mkhulisi <fakudzemkhulisi@gmail.com>
2023-02-21 12:51:34 -0800

    check if a character is lowercase or otherwise

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * _islower - check if a character is lower or uppercas
+  *
+  * Return: 1 if lowercase 0 otherwise
+  */
+int _islower(char c)
+{
+	
+	return (islower);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 void print_alphabet(void);
 void _putchar(char);
 void print_alphabet_x10(void);
+int _islower(int c);
