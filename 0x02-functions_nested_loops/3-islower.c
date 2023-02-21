a629bcb7e605389ad22f7f1440a1e116d288c696
mkhulisi <fakudzemkhulisi@gmail.com>
2023-02-21 13:17:08 -0800

    check if a character is lowercase or otherwise, fixed betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
   * _islower - check if a character is lower or uppercas
-  *
+  *@c: the character to check
   * Return: 1 if lowercase 0 otherwise
   */
 int _islower(int c)
