ac5e885d3391fd2af9a6e644b380d7e67395ec0b
mkhulisi <fakudzemkhulisi@gmail.com>
2023-02-21 10:28:20 -0800

    print alphabets using a function that is user defined

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void print_alphabet(void);
+
+/**
+ * main - program entry poit
+ * program starts from here
+ * Return: 0 allways for success
+ */
+int main(void)
+{
+
+	print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet - function prototype
+ * prints alphabets in lower case
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+**0x02-functions_nested_loops**
+File - 1-alphabet.c
+Description -Write a function that prints the alphabet, in lowercase, followed by a new line. Prototype: void print_alphabet(void); You can only use _putchar twice in your code
