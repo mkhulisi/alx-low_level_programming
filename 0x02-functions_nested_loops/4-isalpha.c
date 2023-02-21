79c8cad8cf0441241701631c5cfeef34670430a5
mkhulisi <fakudzemkhulisi@gmail.com>
2023-02-21 13:24:02 -0800

    check if a character is an alphabet or not

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
   *@c: the character to check
   * Return: 1 if character is alphabet 0 otherwise
   */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
