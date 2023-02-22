3a620e34a0386ce40957925654a18c7fc7b59612
mkhulisi <fakudzemkhulisi@gmail.com>
2023-02-21 17:20:19 -0800

    Get last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * print_last_digit - get the last digit of a number
+  *@r: number to get last digit for
+  * Return: absolute value of of a number
+  */
+int print_last_digit(int r)
+{
+
+	return (r % 10);
+}
+
