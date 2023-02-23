6fb43c42dac8e7290607c987ab7d78b983f3048b
mkhulisi <fakudzemkhulisi@gmail.com>
2023-02-23 04:34:59 -0800

    print a triangle on the terminal

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * print_triangle - Print a triangle, followed by a new line.
+ * @size: the number for parameter
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int x, y = 0;
+	int space;
+
+	if (size > 0)
+	{
+		while (y < size)
+		{
+			space = size - y - 1;
+			while (x < size)
+			{
+				if (space > x)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+				x++;
+			}
+			y++;
+			x = 0;
+			_putchar ('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
