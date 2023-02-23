fe22e424d2534e2ebab72441613d10438147e38e
mkhulisi <fakudzemkhulisi@gmail.com>
2023-02-23 05:11:54 -0800

    print a a number using putchar

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_number - print a number using putchar
+ * @n: the number to be printed
+ * Return: void, nothing
+ */
+void print_number(int n)
+{
+
+	_putchar(45 + n);
+}
