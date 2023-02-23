6a2523a793dff937e8ce5d3e53dcd99fef0c9286
mkhulisi <fakudzemkhulisi@gmail.com>
2023-02-23 01:39:33 -0800

    is uperr or not, fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-#nclude "main.h>
+#nclude "main.h">
 /**
 * _isupper - checks if a a character is upper or lower case
 * @c: alphaetical character to check
@@ -9,8 +9,10 @@ int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
-	return (1);
+		return (1);
 	}
 	else
-	return (0);
+	{
+		return (0);
+	}
 }
