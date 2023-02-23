77ee3512322eec98c4a616dd5673bacd49c40028
mkhulisi <fakudzemkhulisi@gmail.com>
2023-02-23 02:53:06 -0800

    prints straight line on terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_line - prints a staight line using _ character
+ * @n: number controlls how long is line
+ * Return: void, nothing
+ */
+void print_line(int n)
+{
+	int a = 0;
+
+	if (n > 0)
+	{
+		while (a <= n)
+		{
+			_putchar('_');
+			a++;
+		}
+
+	 _putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
