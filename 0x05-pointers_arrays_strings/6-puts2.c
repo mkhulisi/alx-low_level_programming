68926f1883d7011ef85200f00422296211be0264
mkhulisi <fakudzemkhulisi@gmail.com>
2023-02-27 12:16:16 -0800

    Print all characters, fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*puts2 -  prints every other character of a string, starting with the first character
+*puts2 -  prints every other character of a string
 *@str: the pointer to string to print
 *Return: void, nothing
 */
 void puts2(char *str)
 {
-	
+
 	int i = 0;
 
 	while (str[i] != '\0')
