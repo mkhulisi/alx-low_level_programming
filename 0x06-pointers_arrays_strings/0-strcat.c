1a37127fa7c85ea9c82f9fe84dea4ae657c9da6b
mkhulisi <fakudzemkhulisi@gmail.com>
2023-03-01 07:18:55 -0800

    concate two strings fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,35 +1,30 @@
-#include <stdio.h>
-#include "main.h>"
-
+#include "main.h"
 /**
- * _strcat -join two strings and return string pointer
- * @dest: pointer param for primary string
- * @src: string to concatenate to @dest
- * Return: pointer of concated string to update
+ * _strcat - concatenate strings
+ * @dest: first part of string to cancatenate
+ * @src: second string to concatenate
+ *
+ * Return: a pointer containing concatenated sring
  */
 char *_strcat(char *dest, char *src)
 {
-	int dest_size;
-	int src_size;
-	int concate_size;
 	int x;
+	int j;
 
-	dest_size = 0;
-	src_size = 0;
 	x = 0;
-
-	while (dest[dest_size] != '\0')
+	while (dest[x] != '\0')
 	{
-		dest_size++;
+		x++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[x] = src[j];
+		x++;
+		j++;
 	}
 
-	while (src[src_size] != '\0')
-	{
-		src_size++
-	}
+	dest[i] = '\0';
 
-	concate_size = src_size + dest_size;
-
-	while (x <= concate_size)
-	{
-		dest[
+	return (dest);
+}
