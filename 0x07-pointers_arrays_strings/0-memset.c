1f0e73407a1f169525a6cdd74d997328bd6bf7fc
mkhulisi <fakudzemkhulisi@gmail.com>
2023-03-06 08:18:01 -0800

    write constatnt bite to memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+
+#include "main"
+/**
+ * _memset - writes constant bite into memory
+ *
+ * @s: pointer to memory area to write to
+ * @n: the number of bytes to write
+ * @b: constant byte
+ *
+ * Return: a pointer to the memory area @s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	x = 0
+
+	while (x <= n)
+	{
+		s[x] = b;
+		x++;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+**0x07. C - Even more pointers, arrays and strings**
+*File:* 0-memset.c
+*Description:* Write a function that fills memory with a constant byte.
