9a749f56a5bbd927fa8d4dacc1587f9482a6ec2f
mkhulisi <fakudzemkhulisi@gmail.com>
2023-03-08 09:00:41 -0800

    print string in reverse using recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * _print_rev_recursion - print a string in reverse using recursion
+ * @s: pointer to strin to print in reverse
+ * Return: void, nothing
+ */
+
+void _print_rev_recursion(*s)
+{
+	if (*s == '\0')
+	{
+		printf("\n");
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		ptintf("%s", *s);
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -2,3 +2,6 @@
 
 *File:* 0-puts_recursion.c
 *Description:* a function that prints a string, followed by a new line.
+
+*File:* 1-print_rev_recursion.c
+*Description:* a function that prints a string in reverse using regresive call
