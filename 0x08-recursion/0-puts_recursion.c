195c7059c27bee9fc18d1a1f7cfa5ded1bc7e7a9
mkhulisi <fakudzemkhulisi@gmail.com>
2023-03-08 08:35:15 -0800

    print string using recasive call to function

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: points to array of char
+ * Return: void, nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	int x;
+	x = 0;
+
+	if (s[x] == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[x]);
+		_puts_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,4 @@
+**0x08. C - Recursion**
+
+*File:* 0-puts_recursion.c
+*Description:* a function that prints a string, followed by a new line.
