eaa7229430a922422a9908d5cbfa79071750f070
mkhulisi <fakudzemkhulisi@gmail.com>
2023-03-10 02:54:41 -0800

    print program file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program entry point, prints program file name
+ * @argc: contains cli arguments count
+ * @argv: an array containing the actual arguments
+ *Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
